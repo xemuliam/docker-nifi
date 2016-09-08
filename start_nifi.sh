b9944cbff9b4a606671b0f85d95697ece99d961e
Viacheslav Kalashnikov <xemuliam@gmail.com>
2016-09-08 10:47:39 +0300

    NiFi 0.7.0

--- start_nifi.sh
+++ start_nifi.sh
@@ -1,45 +1,62 @@
-#!/bin/sh
+#!/bin/bash
 
-do_s2s_configure() {
+splash() {
+  echo 'Environment:'
+  echo "BANNER_TEXT=$BANNER_TEXT"
+}
 
+configure_common() {
+  sed -i "s/nifi\.ui\.banner\.text=/nifi.ui.banner.text=${BANNER_TEXT}/g" $NIFI_HOME/conf/nifi.properties
+}
+
+configure_site2site() {
+  # configure the receiving end of site2site
   sed -i "s/nifi\.remote\.input\.socket\.host=/nifi.remote.input.socket.host=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.remote\.input\.socket\.port=/nifi.remote.input.socket.port=10101/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.socket\.port=/nifi.remote.input.socket.port=12345/g" $NIFI_HOME/conf/nifi.properties
+  # unsecure for now so we don't complicate the setup with certificates
   sed -i "s/nifi\.remote\.input\.secure=true/nifi.remote.input.secure=false/g" $NIFI_HOME/conf/nifi.properties
 }
 
-do_cluster_manager_configure() {
+configure_cluster_node() {
+  # can't set to 0.0.0.0, as this address is then sent verbatim to NCM, which, in turn
+  # does not resolve it back to the node. If it's set to the ${HOSTNAME}, the cluster works,
+  # but the node's web ui is not accessible on the external network
+  #sed -i "s/nifi\.web\.http\.host=/nifi.web.http.host=0.0.0.0/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.web\.http\.host=/nifi.web.http.host=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.node\.address=/nifi.cluster.node.address=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.node\.protocol\.port=/nifi.cluster.node.protocol.port=12346/g" $NIFI_HOME/conf/nifi.properties
+  # the following properties point to the NCM - note we are using the network alias (implicitly created by docker-compose)
+  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.address=/nifi.cluster.node.unicast.manager.address=ncm/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.protocol\.port=/nifi.cluster.node.unicast.manager.protocol.port=20001/g" $NIFI_HOME/conf/nifi.properties
+}
+
+configure_cluster_manager() {
   sed -i "s/nifi\.web\.http\.host=/nifi.web.http.host=0.0.0.0/g" $NIFI_HOME/conf/nifi.properties
   sed -i "s/nifi\.cluster\.is\.manager=false/nifi.cluster.is.manager=true/g" $NIFI_HOME/conf/nifi.properties
   sed -i "s/nifi\.cluster\.manager\.address=/nifi.cluster.manager.address=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.manager\.protocol\.port=/nifi.cluster.manager.protocol.port=20202/g" $NIFI_HOME/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.manager\.protocol\.port=/nifi.cluster.manager.protocol.port=20001/g" $NIFI_HOME/conf/nifi.properties
 }
 
-do_cluster_node_configure() {
+splash
+configure_common
 
-  sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.address=/nifi.cluster.node.address=${HOSTNAME}/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.protocol\.port=/nifi.cluster.node.protocol.port=30303/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.address=/nifi.cluster.node.unicast.manager.address=ncm/g" $NIFI_HOME/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.protocol\.port=/nifi.cluster.node.unicast.manager.protocol.port=20202/g" $NIFI_HOME/conf/nifi.properties
+# we don't configure acquisition node to serve site-to-site requests,
+# the node initiates push/pull only
 
-}
-
-sed -i "s/nifi\.ui\.banner\.text=/nifi.ui.banner.text=${BANNER_TEXT}/g" $NIFI_HOME/conf/nifi.properties
-
-
-if [ "$INSTANCE_ROLE" == "single-node" ]; then
-  do_s2s_configure
+if [ "$NIFI_INSTANCE_ROLE" == "single-node" ]; then
+  configure_site2site
 fi
 
-if [ "$INSTANCE_ROLE" == "cluster-manager" ]; then
-  do_s2s_configure
-  do_cluster_manager_configure
+if [ "$NIFI_INSTANCE_ROLE" == "cluster-node" ]; then
+  configure_site2site
+  configure_cluster_node
 fi
 
-if [ "$INSTANCE_ROLE" == "cluster-node" ]; then
-  do_s2s_configure
-  do_cluster_node_configure
+if [ "$NIFI_INSTANCE_ROLE" == "cluster-manager" ]; then
+  configure_site2site
+  configure_cluster_manager
 fi
 
-tail -F ${NIFI_HOME}/logs/nifi-app.log &
+# must be an exec so NiFi process replaces this script and receives signals
 ${NIFI_HOME}/bin/nifi.sh run
