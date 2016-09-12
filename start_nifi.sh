5d6c36dc554d9a1c4562245a6f2b01f072bc7530
Viacheslav Kalashnikov <xemuliam@gmail.com>
2016-09-12 10:31:12 +0300

    NiFi 1.0.0

--- start_nifi.sh
+++ start_nifi.sh
@@ -4,14 +4,16 @@ set -e
 
 do_site2site_configure() {
   sed -i "s/nifi\.ui\.banner\.text=/nifi.ui.banner.text=${BANNER_TEXT}/g" ${NIFI_HOME}/conf/nifi.properties
-#  sed -i "s/nifi\.remote\.input\.host=/nifi.remote.input.host=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.host=/nifi.remote.input.host=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.remote\.input\.socket\.port=/nifi.remote.input.socket.port=11111/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.remote\.input\.secure=true/nifi.remote.input.secure=false/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.web\.http\.host=/nifi.web.http.host=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
+#  sed -i "s/nifi\.web\.http\.port=/nifi.web.http.port=8080/g" ${NIFI_HOME}/conf/nifi.properties
 }
 
 do_cluster_node_configure() {
 # NiFi properties
-#  sed -i "s/nifi\.web\.http\.port=/nifi.web.http.port=8080/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.protocol\.is\.secure=true/nifi.cluster.protocol.is.secure=false/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.node\.address=/nifi.cluster.node.address=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.node\.protocol\.port=/nifi.cluster.node.protocol.port=22222/g" ${NIFI_HOME}/conf/nifi.properties
