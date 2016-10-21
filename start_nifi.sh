35e742bc13673addab38ef969b1ab2e7267c6eab
Viacheslav Kalashnikov <xemuliam@users.noreply.github.com>
2016-10-21 14:03:57 +0300

    1.0.0

--- start_nifi.sh
+++ start_nifi.sh
@@ -3,17 +3,17 @@
 set -e
 
 do_site2site_configure() {
-  sed -i "s/nifi\.remote\.input\.host=.*/nifi.remote.input.host=node-${MYID}/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.host=.*/nifi.remote.input.host=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.remote\.input\.socket\.port=.*/nifi.remote.input.socket.port=10101/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.remote\.input\.secure=true/nifi.remote.input.secure=false/g" ${NIFI_HOME}/conf/nifi.properties
 }
 
 do_cluster_node_configure() {
 # NiFi properties
-  sed -i "s/nifi\.web\.http\.host=.*/nifi.web.http.host=node-${MYID}/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.web\.http\.host=.*/nifi.web.http.host=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.protocol\.is\.secure=true/nifi.cluster.protocol.is.secure=false/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.address=.*/nifi.cluster.node.address=node-${MYID}/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.node\.address=.*/nifi.cluster.node.address=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.node\.protocol\.port=.*/nifi.cluster.node.protocol.port=10201/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.zookeeper\.connect\.string=.*/nifi.zookeeper.connect.string=${ZK_NODES_LIST}/g" ${NIFI_HOME}/conf/nifi.properties
   if [ "$USE_EMBEDDED_ZK" == "true" ]; then
@@ -26,8 +26,10 @@ do_cluster_node_configure() {
   sed -i "s/<property name=\"Connect String\">.*</<property name=\"Connect String\">${ZK_NODES_LIST}</g" ${NIFI_HOME}/conf/state-management.xml
 
 # MyId zookeeper
-  mkdir -p ${NIFI_HOME}/state/zookeeper
-  echo ${MYID} > ${NIFI_HOME}/state/zookeeper/myid
+  if [ "$USE_EMBEDDED_ZK" == "true" ]; then
+    mkdir -p ${NIFI_HOME}/state/zookeeper
+    echo ${MYID} > ${NIFI_HOME}/state/zookeeper/myid
+  fi
 
 # Zookeeper properties
 #  sed -i "/^server\.1=/q" ${NIFI_HOME}/conf/zookeeper.properties; sed -i "s/^server\.1=.*/server.1=/g" ${NIFI_HOME}/conf/zookeeper.properties
