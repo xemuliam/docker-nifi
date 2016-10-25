07f2dfa829d5d1ab59a2e0b0676dd5bd98c9f7b3
Viacheslav Kalashnikov <xemuliam@users.noreply.github.com>
2016-10-25 12:15:37 +0300

    1.0.0

--- start_nifi.sh
+++ start_nifi.sh
@@ -4,18 +4,20 @@ set -e
 
 do_site2site_configure() {
   sed -i "s/nifi\.remote\.input\.host=.*/nifi.remote.input.host=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.remote\.input\.socket\.port=.*/nifi.remote.input.socket.port=10101/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.socket\.port=.*/nifi.remote.input.socket.port=2881/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.remote\.input\.secure=true/nifi.remote.input.secure=false/g" ${NIFI_HOME}/conf/nifi.properties
 }
 
 do_cluster_node_configure() {
+  ZK_NODES=$(echo ${ZK_NODES_LIST} | sed -e "s/\s/,/g" -e "s/\(,\)*/\1/g" -e "s/,/:${ZK_CLIENT_PORT},/g" -e "s/$/:ZK_CLIENT_PORT/g")
+
 # NiFi properties
   sed -i "s/nifi\.web\.http\.host=.*/nifi.web.http.host=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.protocol\.is\.secure=true/nifi.cluster.protocol.is.secure=false/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.node\.address=.*/nifi.cluster.node.address=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.protocol\.port=.*/nifi.cluster.node.protocol.port=10201/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.zookeeper\.connect\.string=.*/nifi.zookeeper.connect.string=${ZK_NODES_LIST}/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.node\.protocol\.port=.*/nifi.cluster.node.protocol.port=2882/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.zookeeper\.connect\.string=.*/nifi.zookeeper.connect.string=$ZK_NODES/g" ${NIFI_HOME}/conf/nifi.properties
   if [ "$USE_EMBEDDED_ZK" == "true" ]; then
     sed -i "s/nifi\.state\.management\.embedded\.zookeeper\.start=false/nifi.state.management.embedded.zookeeper.start=true/g" ${NIFI_HOME}/conf/nifi.properties
   else 
@@ -23,7 +25,7 @@ do_cluster_node_configure() {
   fi
 
 # State management
-  sed -i "s/<property name=\"Connect String\">.*</<property name=\"Connect String\">${ZK_NODES_LIST}</g" ${NIFI_HOME}/conf/state-management.xml
+  sed -i "s/<property name=\"Connect String\">.*</<property name=\"Connect String\">$ZK_NODES</g" ${NIFI_HOME}/conf/state-management.xml
 
 # MyId zookeeper
   if [ "$USE_EMBEDDED_ZK" == "true" ]; then
@@ -33,6 +35,8 @@ do_cluster_node_configure() {
 
 # Zookeeper properties
 #  sed -i "/^server\.1=/q" ${NIFI_HOME}/conf/zookeeper.properties; sed -i "s/^server\.1=.*/server.1=/g" ${NIFI_HOME}/conf/zookeeper.properties
+  sed -i "/^server\./,$ d" ${NIFI_HOME}/conf/zookeeper.properties
+  c="1"; IFS=","; for i in ${ZK_NODES_LIST};  do sed -i "\$aserver.$c=$i:2888:3888" ${NIFI_HOME}/conf/zookeeper.properties; ((c++));done
 }
 
 do_site2site_configure
