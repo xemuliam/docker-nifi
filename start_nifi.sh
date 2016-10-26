e17bf373eb48b34ac5eef5a4dce1cbe043d93ed9
Viacheslav Kalashnikov <xemuliam@users.noreply.github.com>
2016-10-26 09:52:29 +0300

    1.0.0

--- start_nifi.sh
+++ start_nifi.sh
@@ -10,40 +10,44 @@ do_site2site_configure() {
 
 do_cluster_node_configure() {
   ZK_NODES=$(echo ${ZK_NODES_LIST} | sed -e "s/\s/,/g" -e "s/\(,\)*/\1/g")
-  ZK_CONNECT_STRING=$(echo $ZK_NODES | sed -e "s/,/:${ZK_CLIENT_PORT},/g" -e "s/$/:${ZK_CLIENT_PORT}/g")
 
-# NiFi properties
+  if [ ! -z "$ZK_CLIENT_PORT" ]; then
+    ZK_CONNECT_STRING=$(echo $ZK_NODES | sed -e "s/,/:${ZK_CLIENT_PORT},/g" -e "s/$/:${ZK_CLIENT_PORT}/g")
+    sed -i "s/clientPort=.*/clientPort=${ZK_CLIENT_PORT}/g" ${NIFI_HOME}/conf/zookeeper.properties
+  else
+    ZK_CONNECT_STRING=$(echo $ZK_NODES | sed -e "s/,/:2181,/g" -e "s/$/:2181/g")
+  fi
+
   sed -i "s/nifi\.web\.http\.host=.*/nifi.web.http.host=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.protocol\.is\.secure=true/nifi.cluster.protocol.is.secure=false/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.node\.address=.*/nifi.cluster.node.address=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.node\.protocol\.port=.*/nifi.cluster.node.protocol.port=2882/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.zookeeper\.connect\.string=.*/nifi.zookeeper.connect.string=$ZK_CONNECT_STRING/g" ${NIFI_HOME}/conf/nifi.properties
-  if [ "$USE_EMBEDDED_ZK" == "true" ]; then
+
+  sed -i "s/<property name=\"Connect String\">.*</<property name=\"Connect String\">$ZK_CONNECT_STRING</g" ${NIFI_HOME}/conf/state-management.xml
+ 
+  if [ ! -z "$ZK_MYID" ]; then
     sed -i "s/nifi\.state\.management\.embedded\.zookeeper\.start=false/nifi.state.management.embedded.zookeeper.start=true/g" ${NIFI_HOME}/conf/nifi.properties
+
+    mkdir -p ${NIFI_HOME}/state/zookeeper
+    echo ${ZK_MYID} > ${NIFI_HOME}/state/zookeeper/myid
   else 
     sed -i "s/nifi\.state\.management\.embedded\.zookeeper\.start=true/nifi.state.management.embedded.zookeeper.start=false/g" ${NIFI_HOME}/conf/nifi.properties
   fi
 
-# State management
-  sed -i "s/<property name=\"Connect String\">.*</<property name=\"Connect String\">$ZK_CONNECT_STRING</g" ${NIFI_HOME}/conf/state-management.xml
-
-# MyId zookeeper
-  if [ "$USE_EMBEDDED_ZK" == "true" ]; then
-    mkdir -p ${NIFI_HOME}/state/zookeeper
-    echo ${ZK_MYID} > ${NIFI_HOME}/state/zookeeper/myid
+  if [ -z "$ZK_INTERCOM_PORT" ]; then
+    ZK_INTERCOM_PORT=2888
+  elif [ -z "$ZK_ELECTION_PORT" ]; then
+    ZK_ELECTION_PORT=3888
   fi
-
-# Zookeeper properties
-  sed -i "s/clientPort=.*/clientPort=${ZK_CLIENT_PORT}/g" ${NIFI_HOME}/conf/zookeeper.properties
   sed -i "/^server\./,$ d" ${NIFI_HOME}/conf/zookeeper.properties
-  
   srv=1; IFS=","; for node in $ZK_NODES; do sed -i "\$aserver.$srv=$node:${ZK_INTERCOM_PORT}:${ZK_ELECTION_PORT}" ${NIFI_HOME}/conf/zookeeper.properties; let "srv+=1"; done
 }
 
 do_site2site_configure
 
-if [ "$INSTANCE_ROLE" == "cluster-node" ]; then
+if [ ! -z "$IS_CLUSTER_NODE" ]; then
   do_cluster_node_configure
 fi
 
