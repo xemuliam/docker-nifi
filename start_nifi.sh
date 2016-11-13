45643ce9fb9ea569a03ad9569f8e0b565793d286
Viacheslav Kalashnikov <xemuliam@users.noreply.github.com>
2016-11-13 12:53:00 +0200

    0.7.1

--- start_nifi.sh
+++ start_nifi.sh
@@ -3,35 +3,43 @@
 set -e
 
 do_site2site_configure() {
-  sed -i "s/nifi\.ui\.banner\.text=.*/nifi.ui.banner.text=${BANNER_TEXT}/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.remote\.input\.socket\.host=.*/nifi.remote.input.socket.host=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.remote\.input\.socket\.port=.*/nifi.remote.input.socket.port=11111/g" ${NIFI_HOME}/conf/nifi.properties
+  if [ -z "$S2S_PORT" ]; then S2S_PORT=2881; fi
+  sed -i "s/nifi\.remote\.input\.host=.*/nifi.remote.input.host=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.remote\.input\.socket\.port=.*/nifi.remote.input.socket.port=${S2S_PORT}/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.remote\.input\.secure=true/nifi.remote.input.secure=false/g" ${NIFI_HOME}/conf/nifi.properties
 }
 
 do_cluster_manager_configure() {
-#  sed -i "s/nifi\.web\.http\.host=.*/nifi.web.http.host=0.0.0.0/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.web\.http\.host=.*/nifi.web.http.host=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.is\.manager=false/nifi.cluster.is.manager=true/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.manager\.address=.*/nifi.cluster.manager.address=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.manager\.protocol\.port=.*/nifi.cluster.manager.protocol.port=22222/g" ${NIFI_HOME}/conf/nifi.properties
+
+  if [ -z "$MANAGER_PROTOCOL_PORT" ]; then MANAGER_PROTOCOL_PORT=2883; fi
+  sed -i "s/nifi\.cluster\.manager\.protocol\.port=.*/nifi.cluster.manager.protocol.port=${MANAGER_PROTOCOL_PORT}/g" ${NIFI_HOME}/conf/nifi.properties
 }
 
 do_cluster_node_configure() {
   sed -i "s/nifi\.web\.http\.host=.*/nifi.web.http.host=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
+  sed -i "s/nifi\.cluster\.protocol\.is\.secure=true/nifi.cluster.protocol.is.secure=false/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.is\.node=false/nifi.cluster.is.node=true/g" ${NIFI_HOME}/conf/nifi.properties
   sed -i "s/nifi\.cluster\.node\.address=.*/nifi.cluster.node.address=${HOSTNAME}/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.address=.*/nifi.cluster.node.unicast.manager.address=ncm/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.protocol\.port=.*/nifi.cluster.node.unicast.manager.protocol.port=22222/g" ${NIFI_HOME}/conf/nifi.properties
-  sed -i "s/nifi\.cluster\.node\.protocol\.port=.*/nifi.cluster.node.protocol.port=33333/g" ${NIFI_HOME}/conf/nifi.properties
+
+  if [ -z "$NODE_PROTOCOL_PORT" ]; then NODE_PROTOCOL_PORT=2882; fi
+  sed -i "s/nifi\.cluster\.node\.protocol\.port=.*/nifi.cluster.node.protocol.port=${NODE_PROTOCOL_PORT}/g" ${NIFI_HOME}/conf/nifi.properties
+
+  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.address=.*/nifi.cluster.node.unicast.manager.address=${NCM}/g" ${NIFI_HOME}/conf/nifi.properties
+  if [ -z "$MANAGER_PROTOCOL_PORT" ]; then MANAGER_PROTOCOL_PORT=2883; fi
+  sed -i "s/nifi\.cluster\.node\.unicast\.manager\.protocol\.port=.*/nifi.cluster.node.unicast.manager.protocol.port=${$MANAGER_PROTOCOL_PORT}/g" ${NIFI_HOME}/conf/nifi.properties
 }
 
+sed -i "s/nifi\.ui\.banner\.text=.*/nifi.ui.banner.text=${BANNER_TEXT}/g" ${NIFI_HOME}/conf/nifi.properties
 do_site2site_configure
 
-if [ "$INSTANCE_ROLE" == "cluster-manager" ]; then
+if [ ! -z "$IS_CLUSTER_MANAGER" ]; then
   do_cluster_manager_configure
 fi
 
-if [ "$INSTANCE_ROLE" == "cluster-node" ]; then
+if [ ! -z "$IS_CLUSTER_NODE" ]; then
   do_cluster_node_configure
 fi
 
